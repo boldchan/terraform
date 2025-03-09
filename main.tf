082f79033ea35bf1fe116f7d9a3f00b9551e638e
peng <paul_chan15@outlook.com>
2025-03-09 14:58:16 +0100

    tterraform hello world

---
+++ main.tf
@@ -0,0 +1,26 @@
+# Configure the Google Cloud provider
+provider "google" {
+  project = "terraform-examples-gcloud"
+  region  = "europe-west1"
+}
+
+# Create a Google Compute instance
+resource "google_compute_instance" "example" {
+  name          = "example"
+  machine_type  = "f1-micro"
+  zone          = "europe-west3-a"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
